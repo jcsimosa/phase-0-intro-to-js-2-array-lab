7fd09f876de384dd4d8e65f97625a26473ddba49
Jose Simosa Nunez <josecarlossimosa@gmail.com>
2022-06-15 11:37:51 -0500

    commit tree 888bdad0f916af552fd2574d4bef372f07b99b5e

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Write your solution here!
+const cats = ["Milo", "Otis","Garfield"];
+
+function destructivelyAppendCat(name){
+   cats.push(name)
+}
+function destructivelyPrependCat(name){
+    cats.unshift(name) 
+};
+function destructivelyRemoveLastCat(){
+    cats.pop()
+};
+function destructivelyRemoveFirstCat(){
+   cats.shift()
+};
+function appendCat(name){
+    return [...cats,name]
+};
+function prependCat(name){
+    let allcats = cats.slice()
+   allcats.unshift(name)
+   return allcats
+ };
+ function removeLastCat(){
+    let allcats = cats.slice()
+    allcats.pop()
+    return allcats
+  };
+  function removeFirstCat(){
+    let allcats = cats.slice()
+    console.log(allcats)
+    allcats.shift()
+    return allcats
+  };
